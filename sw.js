90139c5ac9f44a9d093b6297b652880956ed414e
sam840711-maker <sam840711@gmail.com>
2026-07-09 20:46:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* SH Pilot Logbook service worker */
-var APP_VERSION = 'v0.6u';
+var APP_VERSION = 'v0.6v';
 try { importScripts('version.js'); } catch (e) {}
 const CACHE = 'logbook-' + APP_VERSION;
 const ASSETS = ['./', './index.html', './version.js', './manifest.webmanifest', './icon.svg', './icon-maskable.svg', './lib/jspdf.umd.min.js', './lib/logbook-pdf.js', './lib/roster-sync.js', './lib/airports.js', './lib/font-kr.js'];
